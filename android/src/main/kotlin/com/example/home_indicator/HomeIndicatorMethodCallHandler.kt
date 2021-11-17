49b119f1e0853b6d9383b8770bf75c7581bce737
leo.lu <leo.lu@blackshark.com>
2021-11-17 17:49:40 +0800

    modify Android

--- android/src/main/kotlin/com/example/home_indicator/HomeIndicatorMethodCallHandler.kt
+++ android/src/main/kotlin/com/example/home_indicator/HomeIndicatorMethodCallHandler.kt
@@ -6,12 +6,12 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
 class HomeIndicatorMethodCallHandler: MethodCallHandler {
   override fun onMethodCall(call: MethodCall, result: Result) {
-    when (call.method) {
-      "show" -> result.success(null)
-      "hide" -> result.success(null)
-      "isHidden" -> result.success(null)
-      "deferScreenEdges" -> result.success(null)
-      else -> result.notImplemented()
-    }
+//    when (call.method) {
+//      "show" -> result.success(null)
+//      "hide" -> result.success(null)
+//      "isHidden" -> result.success(null)
+//      "deferScreenEdges" -> result.success(null)
+//      else -> result.notImplemented()
+//    }
   }
 }
